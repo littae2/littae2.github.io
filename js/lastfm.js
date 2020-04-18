c0ec0347f80c7ba26899c265df08205bec23ccb0
Amber Little <littae2@student.op.ac.nz>
2020-04-19 11:21:32 +1200

    added totalListens

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,3 +1,4 @@
+/*
 function allArtists() {
   fetch("json/music.json")
     .then((response) => {
@@ -40,3 +41,49 @@ function topTracks() {
          
 allArtists();
 topTracks();
+*/
+
+function allArtists() {
+  fetch("json/musicv2.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("ArtistList").innerHTML;
+      let output = document.getElementById("ArtistList");
+      let artists = data["artists"];
+      let keys = Object.keys(artists);
+      keys.forEach((k) => {
+        output.innerHTML +=
+          "<img  src='" +
+          artists[k].artistimg +
+          "' width: 200px height: 200px >";
+      });
+    });
+}
+
+function topArtists() {
+  fetch("json/musicv2.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let artistarray = [];
+      document.getElementById("ArtistList").innerHTML;
+      let output = document.getElementById("ArtistList");
+      let artists = data["artists"];
+      let keys = Object.keys(artists);
+      console.log(keys);
+      keys.forEach((k) => {
+        artistarray.push(artists[k]);
+
+        
+      });
+      artistarray.sort[artists.totalListens]
+      console.log(artistarray);
+    });
+    
+}
+function topTracks() {}
+
+topArtists();
