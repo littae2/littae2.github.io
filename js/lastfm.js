1ae298d763e4b0d3e370c3a9e28ca06257f7a860
Amber Little <littae2@student.op.ac.nz>
2020-04-30 15:38:30 +1200

    missed a quote mark, this should now fix images

--- js/lastfm.js
+++ js/lastfm.js
@@ -99,7 +99,7 @@ function topTracks() {
           "<div class = 'track'>" +
           "<img  src='" +
           albumImage +
-          " width=50px height=50px class ='albumTrack'>" +
+          "' width=50px height=50px class ='albumTrack'>" +
           "<div class ='trackInfo'>" +
           songName +
           " - " +
@@ -207,7 +207,7 @@ function recentTracks() {
           "<div class = 'track'>" +
           "<img  src='" +
           albumImage +
-          "width=50px height=50px class ='albumTrack'>" +
+          "' width=50px height=50px class ='albumTrack'>" +
           "<div class ='trackData'>" +
           songName +
           " - " +
